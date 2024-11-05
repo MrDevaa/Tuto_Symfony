61036d26898d3e2c8ab71d9f48f329cb242ca260
Devanandhan <d.codandabany@gmail.com>
2024-11-05 20:46:27 +0100

    added HomeController via cmd

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,14 +1,18 @@
 <?php
 
-    namespace App\Controller;
+namespace App\Controller;
 
-    use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-    use Symfony\Component\
-    use Symfony\
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
 
-    class HomeController {
-
-        function index () {
-
-        }
+class HomeController extends AbstractController
+{
+    #[Route('/home', name: 'app_home')]
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
     }
+}
