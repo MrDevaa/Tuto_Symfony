bed7d64a0e7fefc14bd3e1a2d6c1d21ad23e5d10
Devanandhan <d.codandabany@gmail.com>
2024-11-09 18:08:59 +0100

    base controller and debug:router

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,13 +4,18 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController {
 
     #[Route("/", name: "home")]
-    function index (): Response {
+    function index (Request $request): Response {
         
-        return new Response('Hello world');
+        return new Response('salut je suis ' . $request->query->get('name', 'Iconnu'));
     }
 }
+
+## Analyser un obj par ex:
+## dump() //  vardump() amélioré 
+## dd() // // vardumps amélioré et die;
